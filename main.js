99eb122a87a3c57aec9719a38530244a5271867a
Nongpond <122679268+Nattapon-pond@users.noreply.github.com>
2023-12-28 17:13:22 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,37 +1,33 @@
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<App/>)
-
-function Counter() {
-  return (
-    <div className='counter'>
-      <button> - </button>
-      <h3>0</h3>
-      <button> + </button>
-      <button> C </button>
-      <button> x </button>
-    </div>
-  )
-}
-
-function SumInfo(props)  {
-  const stTitle = {
-    color: props.color,
-    fontSize: props.size === 'big' ? '50px' : '40px'
-  }
-
-  return (
-    <div className='suminfo'>
-      <h1 style={{ color: props.color }}>Sum = 0</h1>
-      <button className='text-center'>Add counter</button>
-    </div>
-  )
-}
-
-function App() {
-  return (
-    <>
-      <SumInfo color="black" size="small"/> 
-      <Counter />
-    </>
-  )
-}
+document.addEventListener("DOMContentLoaded", function () {
+  // Fetch users from an API
+  fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => {
+      const userList = document.querySelector('.user-list');
+      users.forEach(user => {
+        const listItem = document.createElement('li');
+        listItem.textContent = `${user.name} - ${user.email}`;
+        listItem.addEventListener('click', () => {
+          // When a user is clicked, fetch their posts
+          fetch(`https://jsonplaceholder.typicode.com/posts?userId=${user.id}`)
+            .then(response => response.json())
+            .then(posts => {
+              const postInfo = document.querySelector('.post-info');
+              postInfo.innerHTML = ''; // Clear previous posts
+              posts.forEach(post => {
+                const postElement = document.createElement('div');
+                postElement.classList.add('post');
+                postElement.innerHTML = `
+                  <h4>${post.title}</h4>
+                  <p>${post.body}</p>
+                `;
+                postInfo.appendChild(postElement);
+              });
+            })
+            .catch(error => console.error('Error fetching posts:', error));
+        });
+        userList.appendChild(listItem);
+      });
+    })
+    .catch(error => console.error('Error fetching users:', error));
+});
